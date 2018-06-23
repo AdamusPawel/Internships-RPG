5ba3282fe4b742ddbb9a1c9ca7456681f17d5d48
AdamusPawel <adamuspawel95@gmail.com>
2018-06-23 17:51:38 +0200

    initial project re-create

---
+++ Assets/ProjectAssets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(CameraRaycaster))]
+public class CursorAffordance : MonoBehaviour
+{
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D targetCursor = null;
+    [SerializeField] Texture2D unnamedCursor = null;
+    [SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
+
+    CameraRaycaster cameraRaycaster;
+
+    // Use this for initialization
+    void Start()
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        switch (cameraRaycaster.currentLayerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(targetCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unnamedCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                Debug.LogError("Don't know which cursor to show.");
+                return;
+        }
+        
+    }
+}
---
+++ Assets/ProjectAssets/Player/PlayerMovement.cs
@@ -0,0 +1,88 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float walkMoveStopRadius = 0.2f;
+
+    ThirdPersonCharacter thirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+
+    bool isInDirectMode = false;
+
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) // G for Gamepad TODO allow player to remap it later or add
+        {
+            isInDirectMode = !isInDirectMode; // toggle mode
+            currentClickTarget = transform.position; // reset value after swappin' to WASD movement from cursor movement
+        }
+
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+        
+    }
+
+    private void ProcessDirectMovement()
+    {
+        print("Direct movement");
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 movement = v * cameraForward + h * Camera.main.transform.right;
+
+        thirdPersonCharacter.Move(movement, false, false);
+    }
+
+    private void ProcessMouseMovement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //print("Cursor raycast hit layer: " + cameraRaycaster.currentLayerHit);
+            switch (cameraRaycaster.currentLayerHit)
+            {
+                case Layer.Walkable:
+                    currentClickTarget = cameraRaycaster.hit.point; // So not set in default case
+                    break;
+                case Layer.Enemy:
+                    print("Not moving to enemy.");
+                    break;
+                default:
+                    print("Unexpected layer found!");
+                    return;
+            }
+        }
+
+        var playerToClickPoint = currentClickTarget - transform.position;
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
+        {
+            thirdPersonCharacter.Move(playerToClickPoint, false, false);
+        }
+        else
+        {
+            thirdPersonCharacter.Move(Vector3.zero, false, false);
+        }
+    }
+}
+
