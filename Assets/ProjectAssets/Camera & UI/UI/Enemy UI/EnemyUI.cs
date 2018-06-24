5f05f709f6839185e6a1681508b4c2d2b4a1117c
AdamusPawel <adamuspawel95@gmail.com>
2018-06-24 14:42:38 +0200

    fixed enemy health bar to be displayed correctly

--- Assets/ProjectAssets/Camera & UI/UI/Enemy UI/EnemyUI.cs
+++ Assets/ProjectAssets/Camera & UI/UI/Enemy UI/EnemyUI.cs
@@ -1,14 +1,13 @@
 ﻿using UnityEngine;
 
 // Add a UI Socket transform to your enemy
-// Attack this script to the socket
+// Attach this script to the socket
 // Link to a canvas prefab that contains NPC UI
 public class EnemyUI : MonoBehaviour {
 
     // Works around Unity 5.5's lack of nested prefabs
     [Tooltip("The UI canvas prefab")]
-    [SerializeField]
-    GameObject enemyCanvasPrefab = null;
+    [SerializeField] GameObject enemyCanvasPrefab = null;
 
     Camera cameraToLookAt;
 
