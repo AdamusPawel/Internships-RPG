ef3c81ce1b0fc4175ee4e723a0b2ce5ab18ac415
AdamusPawel <adamuspawel95@gmail.com>
2018-06-25 18:14:03 +0200

    Enemies are now following player correctly and stop right in front of player

--- Assets/ProjectAssets/Models and textures/Enemies/Scripts/Enemy.cs
+++ Assets/ProjectAssets/Models and textures/Enemies/Scripts/Enemy.cs
@@ -1,19 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour
 {
 
-    [SerializeField] private float MaxHealthPoints = 100f;
+    [SerializeField] float MaxHealthPoints = 100f;
+    [SerializeField] private float attackRadius = 4f;
+    GameObject player = null;
 
     private float currentHealthPoints = 100f;
+    AICharacterControl aiCharacterControl = null;
 
     public float healthAsPercentage
     {
         get
         {
             return currentHealthPoints / MaxHealthPoints;
+        }
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
         }
     }
 }
