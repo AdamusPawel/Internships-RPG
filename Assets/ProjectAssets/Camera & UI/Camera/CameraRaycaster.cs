b38ea3573306e34ebee329ef9b088d536fb0f1a9
AdamusPawel <adamuspawel95@gmail.com>
2018-06-25 03:49:38 +0200

    fixed cursor minor bug

--- Assets/ProjectAssets/Camera & UI/Camera/CameraRaycaster.cs
+++ Assets/ProjectAssets/Camera & UI/Camera/CameraRaycaster.cs
@@ -54,6 +54,7 @@ public class CameraRaycaster : MonoBehaviour
         // Otherwise return background hit
         raycastHit.distance = distanceToBackground;
         layerHit = Layer.RaycastEndStop;
+        onLayerChange(layerHit);
     }
 
     RaycastHit? RaycastForLayer(Layer layer)
