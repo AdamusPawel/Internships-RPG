b2ff496b0f967091a37ede1a3533a2d6bc36d964
AdamusPawel <adamuspawel95@gmail.com>
2018-06-25 20:21:50 +0200

    fixing player for path finding

--- Assets/Camera & UI/CursorAffordance.cs
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -10,6 +10,8 @@ public class CursorAffordance : MonoBehaviour
     [SerializeField] Texture2D unnamedCursor = null;
     [SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
 
+
+    // TODO solve the fight between serialize and const
     [SerializeField] const int walkableLayerNumber = 8;
     [SerializeField] const int enemyLayerNumber = 9;
 
