0b07a3ca1e399c5d6692dab0f65a46b70610494b
AdamusPawel <adamuspawel95@gmail.com>
2018-06-27 23:53:08 +0200

    fixed bug after swapping from collider to collision type (dmg wasn't been dealt by projectile)

--- Assets/Weapons/Projectile.cs
+++ Assets/Weapons/Projectile.cs
@@ -14,9 +14,9 @@ public class Projectile : MonoBehaviour
         damageCaused = damage;
     }
 
-    void OnTriggerEnter(Collider collider)
+    void OnCollisionEnter(Collision collision)
     {
-        Component damageableComponent = collider.gameObject.GetComponent(typeof(IDamageable));
+        Component damageableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
         print("Damageable component" + damageableComponent);
         if (damageableComponent)
         {
