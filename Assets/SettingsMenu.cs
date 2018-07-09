accb3c5ad76ec336333fbed3f35221e1dcf54bc6
Michał Jakubiec <michalpjakubiec@gmail.com>
2018-07-10 00:48:51 +0200

    ADD: main menu with working volume slider + loading screen

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+}
