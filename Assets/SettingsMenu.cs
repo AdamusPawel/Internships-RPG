df5b3cfe5d02791d8cc9958869172d9b1f327d31
Michał Jakubiec <michalpjakubiec@gmail.com>
2018-07-10 14:44:06 +0200

    ADD: audio imxer, more volume sliders in options

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -6,8 +6,18 @@ public class SettingsMenu : MonoBehaviour
 
     public AudioMixer audioMixer;
 
-    public void SetVolume(float volume)
+    public void SetMasterVolume(float volume)
     {
         audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        audioMixer.SetFloat("SfxVolume", volume);
     }
 }
