dedb40e53addcb22a92727ef9571ff4173bbc6dc
Michał Jakubiec <michalpjakubiec@gmail.com>
2018-07-14 17:31:50 +0200

    UPDATE: rewrite CameraFollow script from scratch

--- Assets/_UI/Camera/CameraFollow.cs
+++ Assets/_UI/Camera/CameraFollow.cs
@@ -1,23 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RPG.CameraUI
 {
     public class CameraFollow : MonoBehaviour
     {
-        GameObject player;
+        public Transform target;
+        public Vector3 offset = new Vector3(0, -1, -1);
 
-        // Use this for initialization
-        void Start()
+        public float pitch = .7f;
+
+        public float minZoom = 4f;
+        public float maxZoom = 13f;
+        public float zoomSpeed = 800f;
+        public float yawSpeed = 100f;
+
+        public float currentZoom = 13f;
+        public float currentYaw = 135f;
+
+        void Update()
         {
-            player = GameObject.FindGameObjectWithTag("Player");
+            currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * Time.deltaTime;
+            currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+            currentYaw += Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
         }
 
-        // Update is called once per frame
         void LateUpdate()
         {
-            transform.position = player.transform.position;
+            transform.position = target.position - offset * currentZoom;
+            transform.LookAt(target.position + Vector3.up * pitch);
+
+            transform.RotateAround(target.position, Vector3.up, currentYaw);
+
+            Debug.Log("Current zoom = " + currentZoom + ", current yaw = " + currentYaw % 360);
         }
     }
 }
