6bb8ae4d709d764457957772ab8d9fa6771922e0
Michał Jakubiec <michalpjakubiec@gmail.com>
2018-08-08 20:17:12 +0200

    EDIT: Main Menu fixes

--- Assets/_Levels/MainMenu/MainMenu.cs
+++ Assets/_Levels/MainMenu/MainMenu.cs
@@ -1,8 +1,52 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    public Toggle fulscreenToggle;
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRate + "Hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width
+                && resolutions[i].height == Screen.height
+                && resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        if (Screen.fullScreen)
+        {
+            fulscreenToggle.isOn = true;
+        }
+        else
+        {
+            fulscreenToggle.isOn = true;
+        }
+    }
+
     public void NewGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -17,5 +61,32 @@ public class MainMenu : MonoBehaviour
     public void QuitGameWrapper()
     {
         QuitGame();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen, resolution.refreshRate);
+    }
+
+    public void SetFullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+        Debug.Log("Fullscreen set to: " + Screen.fullScreen);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        audioMixer.SetFloat("SfxVolume", volume);
     }
 }
