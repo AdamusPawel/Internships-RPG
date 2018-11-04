1e6203cd73af69f35a538337507da45c0fc08f60
AdamusPawel <adamuspawel95@gmail.com>
2018-11-04 21:57:45 +0100

    fixed Quest.cs and added QuestManager.cs

--- Assets/_Characters/Quests/Quest.cs
+++ Assets/_Characters/Quests/Quest.cs
@@ -5,22 +5,21 @@ using UnityEngine;
 [System.Serializable]
 public class Quest
 {
-    enum QuestProgress { NOT_AVAILABLE, AVAILABLE, ACCEPTED, COMPLETE, DONE }
+    public enum QuestProgress { NOT_AVAILABLE, AVAILABLE, ACCEPTED, COMPLETE, DONE }
 
-    string title;                                  // title for the quest
-    int id;                                        // id of quest
-    QuestProgress progress;                        // state of quest (enum)
-    string description;                            // description given by questlog/giver
-    string hint;                                   // hints how to complete quest
-    string summary;                                // 
-    int nextQuest;                                 // if here is any "chain" quest line
+    public string title;                                  // title for the quest
+    public int id;                                        // id of quest
+    public QuestProgress progress;                        // state of quest (enum)
+    public string description;                            // description given by questlog/giver
+    public string hint;                                   // hints how to complete quest
+    public string summary;                                // 
+    public int nextQuest;                                 // if here is any "chain" quest line
 
-    string questObjective;                         // name of the quest objective
-    int questObjectiveCount;                       // current number of them
-    int questObjectiveRequirement;                 // req amount of objectives
+    public string questObjective;                         // name of the quest objective
+    public int questObjectiveCount;                       // current number of them
+    public int questObjectiveRequirement;                 // req amount of objectives
 
-    int expReward;                                 // exp
-    int goldReward;                                // gold
-    string itemReward;                             // items rewarded
-
+    public int expReward;                                 // exp
+    public int goldReward;                                // gold
+    public string itemReward;                             // items 
 }
---
+++ Assets/_Characters/Quests/QuestMenager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class QuestMenager : MonoBehaviour
+{
+
+    public static QuestMenager questManager;
+
+    public List<Quest> questList = new List<Quest>();           // list of all quests
+    public List<Quest> currentQuestList = new List<Quest>();    // list of current quests
+
+    // priv var for our quest objects
+
+    void Awake()
+    {
+        if (questManager == null)
+        {
+            questManager = this;
+        }
+        else if (questManager != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+
+    // ADD ITEMS
+
+    // ACCEPT QUEST
+
+    public void AcceptQuest(int questID)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].id == questID && questList[i].progress == Quest.QuestProgress.AVAILABLE)
+            {
+                currentQuestList.Add(questList[i]);
+                questList[i].progress = Quest.QuestProgress.ACCEPTED;
+            }
+        }
+    }
+
+    // GIVE UP QUEST
+
+    public void GiveUpQuest(int questID)
+    {
+        for (int i = 0; i < currentQuestList.Count; i++)
+        {
+            if (currentQuestList[i].id == questID && currentQuestList[i].progress == Quest.QuestProgress.ACCEPTED)
+            {
+                currentQuestList[i].progress = Quest.QuestProgress.AVAILABLE;
+                currentQuestList[i].questObjectiveCount = 0;
+                currentQuestList.Remove(currentQuestList[i]);
+            }
+        }
+    }
+
+    // COMPLETE QUEST
+    public void CompleteQuest(int questID)
+    {
+        for (int i = 0; i < currentQuestList.Count; i++)
+        {
+            if (currentQuestList[i].id == questID && currentQuestList[i].progress == Quest.QuestProgress.COMPLETE)
+            {
+                currentQuestList[i].progress = Quest.QuestProgress.DONE;
+                currentQuestList.Remove(currentQuestList[i]);
+            }
+        }
+    }
+
+
+    public void AddQuestItem(string questObjective, int itemAmount)
+    {
+        for (int i = 0; i < currentQuestList.Count; i++)
+        {
+            if (currentQuestList[i].questObjective == questObjective &&
+                currentQuestList[i].progress == Quest.QuestProgress.ACCEPTED)
+            {
+                currentQuestList[i].questObjectiveCount += itemAmount;
+            }
+
+            if (currentQuestList[i].questObjectiveCount >= currentQuestList[i].questObjectiveRequirement &&
+                currentQuestList[i].progress == Quest.QuestProgress.ACCEPTED)
+            {
+                currentQuestList[i].progress = Quest.QuestProgress.COMPLETE;
+            }
+        }
+    }
+
+    // REMOVE ITEMS
+
+    // BOOLS
+
+    public bool RequestAvailableQuest(int questID)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].id == questID && questList[i].progress == Quest.QuestProgress.AVAILABLE)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool RequestAcceptedQuest(int questID)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].id == questID && questList[i].progress == Quest.QuestProgress.ACCEPTED)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool RequestCompleteQuest(int questID)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].id == questID && questList[i].progress == Quest.QuestProgress.COMPLETE)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
